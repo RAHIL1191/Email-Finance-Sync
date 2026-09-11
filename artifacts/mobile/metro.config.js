c88fee8ac2fc050fc2136de8e0c7678b76b64893
rahil1191 <rahil1191@gmail.com>
2026-09-10 21:50:25 -0400

    feat: configure metro bundler with custom blocklist for expo environment

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,3 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+const extraBlockList = [
+  /android[\/\\].*/,
+  /\.expo[\/\\].*/,
+];
+
+if (Array.isArray(config.resolver.blockList)) {
+  config.resolver.blockList.push(...extraBlockList);
+} else if (config.resolver.blockList) {
+  config.resolver.blockList = [config.resolver.blockList, ...extraBlockList];
+} else {
+  config.resolver.blockList = extraBlockList;
+}
+
+module.exports = config;
+
